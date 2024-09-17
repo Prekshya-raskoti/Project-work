aa9379d81a14924bb9ce4a7d8f56294135521b83
Prekshya-raskoti <bisuraskoti@gmail.com>
2024-09-17 07:40:34 +0545

    user athentication

--- config/db.js
+++ config/db.js
@@ -1,5 +1,19 @@
+// import mongoose from "mongoose";
+
+//  export const connectDB = async ()=> {
+//     await mongoose.connect('// mongodb+srv://prekshyaraskoti:PreKsShHEllo@cluster0.dy4we.mongodb.net/food ordering web app').then(()=>console.log("DB Connected"));
+// }
 import mongoose from "mongoose";
 
- export const connectDB = async ()=> {
-    await mongoose.connect('// mongodb+srv://prekshyaraskoti:PreKsShHEllo@cluster0.dy4we.mongodb.net/FOOD ORDERING WEB APP').then(()=>console.log("DB Connected"));
-}
+export const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb+srv://prekshyaraskoti:PreKsShHEllo@cluster0.dy4we.mongodb.net/food_ordering_web_app', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("DB Connected");
+  } catch (error) {
+    console.error("Error connecting to DB:", error);
+    process.exit(1); // Exit the process with failure
+  }
+};
--- server.js
+++ server.js
@@ -2,8 +2,9 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/useRoute.js"
 
-
+import 'dotenv/config.js'
 
 // app config
 const app = express()
@@ -18,6 +19,9 @@ connectDB();
 
 //api endpoints
 app.use("/api/food",foodRouter)
+app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
+
 
 app.get("/",(req,res)=>{
     res.send("API Working")
