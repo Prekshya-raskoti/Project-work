53a33057e1368dd048119b3339b006d02fa9cb30
Prekshya-raskoti <bisuraskoti@gmail.com>
2024-09-19 17:22:20 +0545

    commit

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,36 +1,29 @@
-import React, { useState } from 'react'
-import Navbar from './components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
-import Cart from './pages/Cart/Cart'
-import Home from './pages/Home/Home'
-import LoginPopup from './components/LoginPopup/LoginPopup'
-import Footer from './components/footer/footer'
-// import ContactUs from './components/ContactUs/ContactUs'
+import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
+import Cart from './pages/Cart/Cart';
+import Home from './pages/Home/Home';
+import LoginPopup from './components/LoginPopup/LoginPopup';
+import Footer from './components/footer/footer';
 
 const App = () => {
-    const [showLogin,setShowLogin] = useState(false)
-  
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
-  <>
-    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
-        {/* {showLogin?<ContactUs setShowLogin={setShowLogin}}/>:<></>} */}
+    <>
+      {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
+      <div className='app'>
+        <Navbar setShowLogin={setShowLogin} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/order' element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-
-    <div className='app'>
-      
-<Navbar setShowLogin={setShowLogin}/>
-<Routes>
-<Route path='/' element={<Home/>} />
-<Route path='/cart' element={<Cart/>} />
-<Route path='/order' element={<PlaceOrder/>} />
-{/* <Route path='/contactUs' element={<ContactUs/>}/> */}
-</Routes>
-</div>
-<Footer/>
-</>
-    
-  )
-}
-
-export default App
+export default App;
--- front-end/src/components/khalti/KhaltiKey.jsx
+++
@@ -1,3 +0,0 @@
-let myKey = {
-    publicTestKey: ""
-}
